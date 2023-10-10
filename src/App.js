fe5940a9bd8008633a282142afa91c702ed1f7e6
hjyang369 <ghlwls6@gmail.com>
2023-10-11 01:51:23 +0900

    add: 공식문서 틱택톡 구현중

--- src/App.js
+++ src/App.js
@@ -8,17 +8,50 @@ const Square = ({ value, handleClick }) => {
   );
 };
 
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
+const Board = ({ squares, handlePlay, xIsNext }) => {
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
 
   function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) return;
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
-    setSquares(nextSquares);
+    xIsNext ? (nextSquares[i] = "X") : (nextSquares[i] = "O");
+    handlePlay(nextSquares);
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} handleClick={() => handleClick(0)} />
         <Square value={squares[1]} handleClick={() => handleClick(1)} />
@@ -38,4 +71,30 @@ const Board = () => {
   );
 };
 
-export default Board;
+const Game = () => {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const currentSquares = squares[squares.length - 1];
+
+  const handlePlay = (nextSquares) => {
+    setSquares([...squares, nextSquares]);
+    setXIsNext(!xIsNext);
+  };
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board
+          squares={currentSquares}
+          handlePlay={handlePlay}
+          xIsNext={xIsNext}
+        />
+      </div>
+      <div className="game-info">
+        <ol>{handlePlay}</ol>
+      </div>
+    </div>
+  );
+};
+
+export default Game;
